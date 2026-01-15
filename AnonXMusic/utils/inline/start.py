80653f1668d6ec74c2aecf1bcdf5a25b4b94ac2e
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-15 11:00:58 +0530

    0.3 version

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -20,20 +20,30 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text="✚ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ✚",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(
+                text="sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT
+            ),
+            InlineKeyboardButton(
+                text="ᴄʜᴀɴɴᴇʟ", url=config.SUPPORT_CHANNEL
+            ),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text="sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url=f"https://t.me/about_deadly_venom"
+            ),
+            InlineKeyboardButton(
+                text="ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=config.OWNER_ID
+            ),
         ],
-        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://t.me/about_deadly_venom")],
-    
+        [
+            InlineKeyboardButton(
+                text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="settings_back_helper"
+            )
+        ],
     ]
-    
     return buttons
