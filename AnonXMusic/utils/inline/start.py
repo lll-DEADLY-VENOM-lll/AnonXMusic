2bf34c3ec096e798da47f0d965b128850f195f9e
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-26 12:28:40 +0530

    0.3 version

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -3,7 +3,7 @@ import config
 from AnonXMusic import app
 
 def start_panel(_):
-    # Yeh buttons tab dikhenge jab bot GROUP mein /start kiya jaye
+    # Yeh buttons tab dikhenge jab bot group mein start kiya jaye
     buttons = [
         [
             InlineKeyboardButton(
@@ -17,25 +17,32 @@ def start_panel(_):
     ]
     return buttons
 
+
 def private_panel(_):
-    # Yeh buttons tab dikhenge jab bot PRIVATE (DM) mein /start kiya jaye
+    # Error fix: Check if OWNER_ID is a list or a single integer
+    if isinstance(config.OWNER_ID, list):
+        owner_id = config.OWNER_ID[0]
+    else:
+        owner_id = config.OWNER_ID
+
+    # Yeh buttons tab dikhenge jab bot private (DM) mein start kiya jaye
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"], # Text: Add me to your group
+                text=_["S_B_3"], # Add me to your group
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"), # Text: Help & Commands
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"), # Help & Commands
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT), # Support
-            InlineKeyboardButton(text="🛠 ᴅᴇᴠ", user_id=config.OWNER_ID[0]), # Owner Profile
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT), # Support Group
+            InlineKeyboardButton(text="🛠 ᴅᴇᴠ", user_id=owner_id), # Owner link (Fixed)
         ],
         [
-            InlineKeyboardButton(text="❄️ sᴏᴜʀᴄᴇ", url=f"https://t.me/HEROKU_CLUB"), # Source Code link
-            InlineKeyboardButton(text="💌 ʏᴛ-ᴀᴘɪ", callback_data="bot_info_data"), # Extra Info
+            InlineKeyboardButton(text="❄️ sᴏᴜʀᴄᴇ", url=f"https://t.me/HEROKU_CLUB"),
+            InlineKeyboardButton(text="💌 ʏᴛ-ᴀᴘɪ", callback_data="bot_info_data"),
         ],
     ]
     return buttons
