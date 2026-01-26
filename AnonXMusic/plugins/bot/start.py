46847a6964442b9abcb41470490d48e1a00c1055
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-26 11:11:17 +0530

    0.3 version

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -1,46 +1,44 @@
-import time
-import re
 import random
-import asyncio
+import time
 
+from py_yt import VideosSearch
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from pyrogram.errors.exceptions.not_acceptable_406 import ChannelPrivate
-from pyrogram.errors.exceptions.flood_420 import SlowmodeWait
-from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import _boot_
-from AnonXMusic.plugins.sudo.sudoers import sudoers_list
-from AnonXMusic.utils.database import (
-    add_served_chat,
-    add_served_user,
-    blacklisted_chats,
-    get_lang,
-    is_banned_user,
-    is_on_off,
-    blacklist_chat,
-)
-from AnonXMusic.utils.decorators.language import LanguageStart
-from AnonXMusic.utils.formatters import get_readable_time
-from AnonXMusic.utils.inline import help_pannel, private_panel, start_panel
-from config import BANNED_USERS, LOGGER_ID
+from AloneMusic import app
+from AloneMusic.misc import _boot_
+from AloneMusic.plugins.sudo.sudoers import sudoers_list
+from AloneMusic.utils.database import (add_served_chat, add_served_user,
+                                       blacklisted_chats, get_lang,
+                                       is_banned_user, is_on_off)
+from AloneMusic.utils.decorators.language import LanguageStart
+from AloneMusic.utils.formatters import get_readable_time
+from AloneMusic.utils.inline import help_pannel, private_panel, start_panel
+from config import BANNED_USERS
 from strings import get_string
+
+EFFECT_ID = [
+    5046509860389126442,
+    5107584321108051014,
+    5104841245755180586,
+    5159385139981059251,
+]
 
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
+    await message.react("🍓")
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0CdQO5IgACMTplUFOpwDjf-UC7pqVt9uG659qxWQACfQkAAghYGFVtSkRZ5FZQXDME")
             return await message.reply_photo(
-                photo=random.choice(config.START_IMG_URL),
+                photo=config.START_IMG_URL,
+                has_spoiler=True,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -81,6 +79,7 @@ async def start_pm(client, message: Message, _):
             await app.send_photo(
                 chat_id=message.chat.id,
                 photo=thumbnail,
+                has_spoiler=True,
                 caption=searched_text,
                 reply_markup=key,
             )
@@ -91,9 +90,10 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgUAAx0CdQO5IgACMTplUFOpwDjf-UC7pqVt9uG659qxWQACfQkAAghYGFVtSkRZ5FZQXDME")
         await message.reply_photo(
-            photo=random.choice(config.START_IMG_URL),
+            photo=config.START_IMG_URL,
+            has_spoiler=True,
+            message_effect_id=random.choice(EFFECT_ID),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -109,26 +109,13 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    try:
-        await message.reply_photo(
-        photo=random.choice(config.START_IMG_URL),
+    await message.reply_photo(
+        photo=config.START_IMG_URL,
+        has_spoiler=True,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
-        return await add_served_chat(message.chat.id)
-    except ChannelPrivate:
-        return
-    except SlowmodeWait as e:
-        asyncio.sleep(e.value)
-        try:
-            await message.reply_photo(
-        photo=random.choice(config.START_IMG_URL),
-        caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
-        reply_markup=InlineKeyboardMarkup(out),
-        )
-            return await add_served_chat(message.chat.id)
-        except:
-            return
+    return await add_served_chat(message.chat.id)
 
 
 @app.on_message(filters.new_chat_members, group=-1)
@@ -156,18 +143,11 @@ async def welcome(client, message: Message):
                         disable_web_page_preview=True,
                     )
                     return await app.leave_chat(message.chat.id)
-                
-                ch = await app.get_chat(message.chat.id)
-                if (ch.title and re.search(r'[\u1000-\u109F]', ch.title)) or \
-                    (ch.description and re.search(r'[\u1000-\u109F]', ch.description)):
-                        await blacklist_chat(message.chat.id)
-                        await message.reply_text("This group is not allowed to play songs")
-                        await app.send_message(LOGGER_ID, f"This group has been blacklisted automatically due to myanmar characters in the chat title, description or message \n Title:{ch.title} \n ID:{message.chat.id}")
-                        return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    photo=random.choice(config.START_IMG_URL),
+                    photo=config.START_IMG_URL,
+                    has_spoiler=True,
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
