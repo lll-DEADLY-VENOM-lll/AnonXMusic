23852ac485dc1478ae14f68cdcbc23f635f654f3
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-31 07:12:26 +0530

    0.3 version

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -1,13 +1,10 @@
 import sys
 from pyrogram import Client
-
 import config
-
 from ..logging import LOGGER
 
 assistants = []
 assistantids = []
-
 
 class Userbot(Client):
     def __init__(self):
@@ -49,123 +46,120 @@ class Userbot(Client):
 
     async def start(self):
         LOGGER(__name__).info(f"Starting Assistants...")
+        
+        # Assistant 1
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("GOD_HYPER_O_P")
                 await self.one.join_chat("GOD_HYPER_O_P")
             except:
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.one.send_message(config.LOGGER_ID, "Assistant 1 Started")
             except:
-                LOGGER(__name__).error(
-                    "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
-                )
-                exit()
+                LOGGER(__name__).error("Assistant Account 1 has failed to access the log Group.")
+            
             self.one.id = self.one.me.id
             self.one.name = self.one.me.mention
+            # Username check bypass
             if not self.one.me.username:
-                LOGGER(__name__).error("Please set username to assistants and restart the bot again")
-                sys.exit()
-            self.one.username = self.one.me.username
+                self.one.username = "No_Username"
+            else:
+                self.one.username = self.one.me.username
+            
             assistantids.append(self.one.id)
             LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
 
+        # Assistant 2
         if config.STRING2:
             await self.two.start()
             try:
                 await self.two.join_chat("GOD_HYPER_O_P")
-                await self.one.join_chat("GOD_HYPER_O_P")
             except:
                 pass
             assistants.append(2)
             try:
-                await self.two.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.two.send_message(config.LOGGER_ID, "Assistant 2 Started")
             except:
-                LOGGER(__name__).error(
-                    "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
-                )
-                exit()
+                LOGGER(__name__).error("Assistant Account 2 has failed to access the log Group.")
+            
             self.two.id = self.two.me.id
             self.two.name = self.two.me.mention
             if not self.two.me.username:
-                LOGGER(__name__).error("Please set username to assistants and restart the bot again")
-                sys.exit()
-            self.two.username = self.two.me.username
+                self.two.username = "No_Username"
+            else:
+                self.two.username = self.two.me.username
+            
             assistantids.append(self.two.id)
             LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
 
+        # Assistant 3
         if config.STRING3:
             await self.three.start()
             try:
                 await self.three.join_chat("GOD_HYPER_O_P")
-                await self.one.join_chat("GOD_HYPER_O_P")
             except:
                 pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.three.send_message(config.LOGGER_ID, "Assistant 3 Started")
             except:
-                LOGGER(__name__).error(
-                    "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
-                )
-                exit()
+                LOGGER(__name__).error("Assistant Account 3 has failed to access the log Group.")
+            
             self.three.id = self.three.me.id
             self.three.name = self.three.me.mention
             if not self.three.me.username:
-                LOGGER(__name__).error("Please set username to assistants and restart the bot again")
-                sys.exit()
-            self.three.username = self.three.me.username
+                self.three.username = "No_Username"
+            else:
+                self.three.username = self.three.me.username
+            
             assistantids.append(self.three.id)
             LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}")
 
+        # Assistant 4
         if config.STRING4:
             await self.four.start()
             try:
                 await self.four.join_chat("GOD_HYPER_O_P")
-                await self.one.join_chat("GOD_HYPER_O_P")
             except:
                 pass
             assistants.append(4)
             try:
-                await self.four.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.four.send_message(config.LOGGER_ID, "Assistant 4 Started")
             except:
-                LOGGER(__name__).error(
-                    "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
-                )
-                exit()
+                LOGGER(__name__).error("Assistant Account 4 has failed to access the log Group.")
+            
             self.four.id = self.four.me.id
             self.four.name = self.four.me.mention
             if not self.four.me.username:
-                LOGGER(__name__).error("Please set username to assistants and restart the bot again")
-                sys.exit()
-            self.four.username = self.four.me.username
+                self.four.username = "No_Username"
+            else:
+                self.four.username = self.four.me.username
+            
             assistantids.append(self.four.id)
             LOGGER(__name__).info(f"Assistant Four Started as {self.four.name}")
 
+        # Assistant 5
         if config.STRING5:
             await self.five.start()
             try:
                 await self.five.join_chat("HEROKU_CLUB")
-                await self.one.join_chat("HEROKU_CLUB")
             except:
                 pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.five.send_message(config.LOGGER_ID, "Assistant 5 Started")
             except:
-                LOGGER(__name__).error(
-                    "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
-                )
-                exit()
+                LOGGER(__name__).error("Assistant Account 5 has failed to access the log Group.")
+            
             self.five.id = self.five.me.id
             self.five.name = self.five.me.mention
             if not self.five.me.username:
-                LOGGER(__name__).error("Please set username to assistants and restart the bot again")
-                sys.exit()
-            self.five.username = self.five.me.username
+                self.five.username = "No_Username"
+            else:
+                self.five.username = self.five.me.username
+            
             assistantids.append(self.five.id)
             LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
 
