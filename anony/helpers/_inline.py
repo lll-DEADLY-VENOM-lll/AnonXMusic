a567fe5e60bf8fce7fe090b4d47d89c75f590885
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-04-06 13:49:58 +0530

    0.3 version

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -1,21 +1,7 @@
-# # File: anony/helpers/_inline.py
-from pyrogram import types  # <--- ADD THIS IMPORT
-from .markup.start import StartMarkup
-from .markup.play import PlayMarkup
-from anony import config
+# __init__.py 
+from .start import StartPanel
+from .play import PlayPanel
 
-class Inline(StartMarkup, PlayMarkup):
+class Inline(StartPanel, PlayPanel):
     def __init__(self):
         super().__init__()
-
-    def ping_markup(self, text: str) -> types.InlineKeyboardMarkup:
-        return self.ikm([[self.ikb(text=f"🚀 {text}", url=config.SUPPORT_CHAT)]])
-
-    def yt_key(self, link: str) -> types.InlineKeyboardMarkup:
-        return self.ikm([[
-            self.ikb(text="📋 Copy Link", copy_text=link),
-            self.ikb(text="📺 YouTube", url=link),
-        ]])
-
-    def cancel_dl(self, text: str) -> types.InlineKeyboardMarkup:
-        return self.ikm([[self.ikb(text=f"❌ {text}", callback_data="cancel_dl")]])
